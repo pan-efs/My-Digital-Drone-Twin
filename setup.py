430f89966afd2c4dea047cca6a54bb0c13f5f72c
Panagiotis Efstratiou <pefstrat@gmail.com>
2021-01-24 20:59:31 +0100

    Changed packages

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="my_drone_twin",
+    name="my_digital_drone_twin",
     version="0.0.1",
-    description="3D Human pose estimation and extract biomechanics info",
-    url="",
+    description="3D Human pose estimation and extract biomechanics info using a depth camera attached on drone",
+    url="https://github.com/pan-efs/My-Digital-Drone-Twin",
     author="Panagiotis Efstratiou",
-    author_email="pefstrat@gmail.com",
+    author_email="",
     license="Creative Commons Zero v1.0 Universal",
     packages=[
-        "pose_estimation/convert-bagfile-skel",
-        "biomechanics/biomechanics2D",
-        "biomechanics/biomechanics3D",
     ],
-    install_requires=["pandas", "numpy", "math",],
+    install_requires=["pandas", "numpy", "pyrealsense2"],
     zip_safe=False,
 )
