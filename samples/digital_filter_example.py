443d76246d95c3f8b9806c3cf4a6c1be34a44481
pan-efs <pefstrat@gmail.com>
2021-02-15 22:06:48 +0100

    Expand the sample

--- samples/digital_filter_example.py
+++ samples/digital_filter_example.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import *
 from filters.digital_filter import DigitalFilter as DigitalFilter
+from biomechanics.biomechanics2D import AngularKinematics as AngularKinematics
+from biomechanics.biomechanics2D import LinearKinematics as LinearKinematics
 
 #
 # This file contains how we can apply digital filter (IIR or FIR) on a sequence of data.
@@ -84,22 +86,107 @@ for i in range(0, len(ankle_right_array)):
 # Create DigitalFilter object
 f = DigitalFilter()
 
+# Left side of lower body
+# hipLeftX
+z, z2, y_hlx = f.digital_filter(hipLeftX, 3)
+f.visualization(hipLeftX, z, z2, y_hlx, 'Hip Left X axis')
+f.visualize_local_max_min(y_hlx, 'Hip Left X')
+
+# hipLeftY
+z, z2, y_hly = f.digital_filter(hipLeftY, 3)
+f.visualization(hipLeftY, z, z2, y_hly, 'Hip Left Y axis')
+f.visualize_local_max_min(y_hly, 'Hip Left Y')
+
 # kneeLeftX
-z, z2, y = f.digital_filter(kneeLeftX, 3)
-f.visualization(kneeLeftX, z, z2, y, 'Knee Left X axis')
-f.visualize_local_max_min(y, 'Knee Left X')
+z, z2, y_klx = f.digital_filter(kneeLeftX, 3)
+f.visualization(kneeLeftX, z, z2, y_klx, 'Knee Left X axis')
+f.visualize_local_max_min(y_klx, 'Knee Left X')
 
 # kneeLeftY
-z, z2, y = f.digital_filter(kneeLeftY, 3)
-f.visualization(kneeLeftY, z, z2, y, 'Knee Left Y axis')
-f.visualize_local_max_min(y, 'Knee Left Y')
+z, z2, y_kly = f.digital_filter(kneeLeftY, 3)
+f.visualization(kneeLeftY, z, z2, y_kly, 'Knee Left Y axis')
+f.visualize_local_max_min(y_kly, 'Knee Left Y')
 
 # ankleLeftX
-z, z2, y = f.digital_filter(ankleLeftX, 3)
-f.visualization(ankleLeftX, z, z2, y, 'Ankle Left X axis')
-f.visualize_local_max_min(y, 'Ankle Left X')
+z, z2, y_alx = f.digital_filter(ankleLeftX, 3)
+f.visualization(ankleLeftX, z, z2, y_alx, 'Ankle Left X axis')
+f.visualize_local_max_min(y_alx, 'Ankle Left X')
 
 # ankleLeftY
-z, z2, y = f.digital_filter(ankleLeftY, 3)
-f.visualization(ankleLeftY, z, z2, y, 'Ankle Left Y axis')
-f.visualize_local_max_min(y, 'Ankle Left Y')
+z, z2, y_aly = f.digital_filter(ankleLeftY, 3)
+f.visualization(ankleLeftY, z, z2, y_aly, 'Ankle Left Y axis')
+f.visualize_local_max_min(y_aly, 'Ankle Left Y')
+
+# Right side of lower body
+# hipRightX
+z, z2, y_hrx = f.digital_filter(hipRightX, 3)
+f.visualization(hipRightX, z, z2, y_hrx, 'Hip Right X axis')
+f.visualize_local_max_min(y_hrx, 'Hip Right X')
+
+# hipRightY
+z, z2, y_hry = f.digital_filter(hipRightY, 3)
+f.visualization(hipRightY, z, z2, y_hry, 'Hip Right Y axis')
+f.visualize_local_max_min(y_hry, 'Hip Right Y')
+
+# kneeRightX
+z, z2, y_krx = f.digital_filter(kneeRightX, 3)
+f.visualization(kneeRightX, z, z2, y_krx, 'Knee Right X axis')
+f.visualize_local_max_min(y_krx, 'Knee Right X')
+
+# kneeRightY
+z, z2, y_kry = f.digital_filter(kneeRightY, 3)
+f.visualization(kneeRightY, z, z2, y_kry, 'Knee Right Y axis')
+f.visualize_local_max_min(y_kry, 'Knee Right Y')
+
+# ankleRightX
+z, z2, y_arx = f.digital_filter(ankleRightX, 3)
+f.visualization(ankleRightX, z, z2, y_arx, 'Ankle Right X axis')
+f.visualize_local_max_min(y_arx, 'Ankle Right X')
+
+# ankleRightY
+z, z2, y_ary = f.digital_filter(ankleRightY, 3)
+f.visualization(ankleRightY, z, z2, y_ary, 'Ankle Right Y axis')
+f.visualize_local_max_min(y_ary, 'Ankle Right Y')
+
+#-------Biomechanics part-------#
+
+# Convert to 2D arrays as before
+hip_left_filtered = np.vstack((y_hlx, y_kly)).T
+knee_left_filtered = np.vstack((y_klx, y_kly)).T
+ankle_left_filtered = np.vstack((y_alx, y_aly)).T
+
+hip_right_filtered = np.vstack((y_hrx, y_hry)).T
+knee_right_filtered = np.vstack((y_krx, y_kry)).T
+ankle_right_filtered = np.vstack((y_arx, y_ary)).T
+
+# Create AngularKinematics object
+k = AngularKinematics()
+
+# Normalize coordinates according to image height and width
+hip_right_filtered_normalized = k.normalize_2d_coordinates(hip_right_filtered, 1280, 720)
+knee_right_filtered_normalized = k.normalize_2d_coordinates(knee_right_filtered, 1280, 720)
+ankle_right_filtered_normalized = k.normalize_2d_coordinates(ankle_right_filtered, 1280, 720)
+
+hip_left_filtered_normalized = k.normalize_2d_coordinates(hip_left_filtered, 1280, 720)
+knee_left_filtered_normalized = k.normalize_2d_coordinates(knee_left_filtered, 1280, 720)
+ankle_left_filtered_normalized = k.normalize_2d_coordinates(ankle_left_filtered, 1280, 720)
+
+# Calculate the angles
+r_theta = k.calculate_relative_angle(hip_right_filtered_normalized, knee_right_filtered_normalized, ankle_right_filtered_normalized)
+l_theta = k.calculate_relative_angle(hip_left_filtered_normalized, knee_left_filtered_normalized, ankle_left_filtered_normalized)
+
+# Drop last row of time for equality reasons
+time1 = time.loc[0:191]
+
+# Visualisation of knee angles
+fig, (ax1, ax2) = plt.subplots(1,2)
+ax1.plot(time1, r_theta)
+ax1.set_title('Knee angle (Right)')
+ax1.set(xlabel = 'Time (s)', ylabel = 'Knee angle (degrees)')
+
+ax2.plot(time1, l_theta)
+ax2.set_title('Knee angle (Left)')
+ax2.set(xlabel = 'Time (s)', ylabel = 'Knee angle (degrees)')
+
+# Filtered figure
+plt.show()
