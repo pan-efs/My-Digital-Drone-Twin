c4fd1843dd9876001828c229626f6d8860abc440
pan-efs <pefstrat@gmail.com>
2021-02-15 15:38:19 +0100

    Add new class

---
+++ filters/digital_filter.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from scipy.signal import *
+
+class DigitalFilter:
+    
+    def digital_filter(self, xn, n):
+        b, a = butter(n, 0.05)
+        zi = lfilter_zi(b, a)
+        z, _ = lfilter(b, a, xn, zi = zi * xn[0])
+        z2, _ = lfilter(b, a, z, zi = zi * z[0])
+        y = filtfilt(b, a, xn)
+        
+        return z, z2, y
+    
+    def visualization(self, signal, z, z2, y, title: str):
+        plt.figure
+        plt.plot(signal, 'b', alpha = 0.75)
+        plt.plot(z, 'r--', z2, 'r', y, 'k')
+        plt.legend(('noisy signal', 'lfilter, once', 'lfilter, twice',
+            'filtfilt'), loc = 'best')
+        plt.title(title)
+        plt.xlabel('Frames')
+        plt.ylabel('coords')
+        plt.grid(True)
+        plt.show()
