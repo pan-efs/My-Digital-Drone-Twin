3c7b57f0c45f3849d96a4713e4efa5ffe03aed9d
pan-efs <pefstrat@gmail.com>
2021-03-28 23:18:31 +0200

    Update, new folder

--- animations/anime.py
+++ animations/anime.py
@@ -1,18 +1,22 @@
-from matplotlib import pyplot as plt
 import numpy as np
+import os
+import moviepy.editor as mp
+from matplotlib import pyplot as plt
 import matplotlib.animation as animation
+from configs.configuration import Configuration 
 
 class Animations:
-    def __init__(self, angle_list: list):
+    def __init__(self, angle_list: list, title: str):
         self.angle = angle_list
         self.point_to_show = 3
+        self.title = title
     
     def update(self, i):
         new_data = self.angle[i : i + self.point_to_show]
         self.line.set_ydata(new_data)
         return self.line,
     
-    def anime_plot(self):
+    def anime_plot(self, fps: int):
         my_list = self.angle
         
         fig, ax = plt.subplots()
@@ -25,8 +29,19 @@ class Animations:
         plt.xticks([])
         plt.grid(True)
         
-        ani = animation.FuncAnimation(fig, self.update, 
+        self.ani = animation.FuncAnimation(fig, self.update, 
                                     frames = len(self.angle) - self.point_to_show, 
-                                    interval = 30)
+                                    interval = fps)
         
         plt.show()
+    
+    def save_as_gif(self):
+        main_path = Configuration()._get_dir('main')
+        self.ani.save(main_path + 'animations\\videos\\' + self.title + '.gif', 
+                    writer = 'PillowWriter', fps = 30)
+    
+    def gif_to_mp4(self):
+        main_path = Configuration()._get_dir('main')
+        os.chdir(main_path + 'animations\\videos\\')
+        clip = mp.VideoFileClip(self.title + '.gif')
+        clip.write_videofile(self.title + '.mp4')
