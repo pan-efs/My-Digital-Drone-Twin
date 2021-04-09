542ff7c71eb33c81256e6b0669b07d22067305a5
pan-efs <pefstrat@gmail.com>
2021-04-09 15:43:12 +0200

    Improve docstrings

--- filters/digital_filter.py
+++ filters/digital_filter.py
@@ -4,19 +4,19 @@ import pandas as pd
 from scipy.signal import *
 
 class DigitalFilter:
-    """
-    Description: [text]:
-    Filter data along one-dimension with an IIR or FIR filter.
-    Filter a data sequence, xn, using a digital filter. 
-    This works for many fundamental data types (including Object type). 
-    The filter is a direct form II transposed implementation of the standard difference equation.
     
-    Parameters: [array, int]: [signal, order]
-    
-    Returns: [z, z2, y]: [lfilter once, lfilter twice, filtfilt]
-            *filtfilt = A forward-backward filter, to obtain a filter with linear phase.
-    """
     def digital_filter(self, xn: np.ndarray, n):
+        """
+        Description: [text]:
+        Design a digital filter.
+        Find the coefficients of initial states.
+        Filter data along one-dimension with an IIR. Filter a data sequence, xn, using a digital filter.
+        Apply a forward-backward filter, to obtain a filter with linear phase.
+    
+        Parameters: [array, int]: [signal, order]
+    
+        Returns: [z, z2, y]: [lfilter once, lfilter twice, filtfilt]
+        """
         b, a = butter(n, 0.05)
         zi = lfilter_zi(b, a)
         z, _ = lfilter(b, a, xn, zi = zi * xn[0])
@@ -25,11 +25,11 @@ class DigitalFilter:
         
         return z, z2, y
     
-    """
-    Description: [text]:
-    Visualize the returned parameters of digital_filter function.
-    """
     def visualization(self, signal: np.ndarray, z, z2, y, title: str):
+        """
+        Description: [text]:
+        Visualize the returned parameters of digital_filter function.
+        """
         plt.figure
         plt.plot(signal, 'b', alpha = 0.75)
         plt.plot(z, 'r--', z2, 'r', y, 'k')
@@ -41,11 +41,11 @@ class DigitalFilter:
         plt.grid(True)
         plt.show()
     
-    """
-    Description: [text]:
-    Visualize filtfilt filtered signal.
-    """
     def visualize_local_max_min(self, filtfilt: np.ndarray, name: str):
+        """
+        Description: [text]:
+        Visualize filtfilt filtered signal.
+        """
         df = pd.DataFrame(filtfilt, columns = ['filtfilt'])
         df['min'] = df.iloc[argrelextrema(df.filtfilt.values, np.less_equal,
                     order=3)[0]]['filtfilt']
