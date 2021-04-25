6f11fdc2185ff6fd8f334d2859af360b3417b06f
pan-efs <pefstrat@gmail.com>
2021-04-25 23:50:13 +0200

    Added __init__

--- filters/moving_average.py
+++ filters/moving_average.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 class MovingAverage:
+    def __init__(self):
+        pass
     
     def moving_average(self, coord, window):
         return np.convolve(coord, np.ones(window), 'valid') / window
