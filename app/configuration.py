2183a8ff2ae03a64ad0f2981d4b75a7f67d9b790
pan-efs <pefstrat@gmail.com>
2021-04-25 23:48:28 +0200

    Changed paths

--- app/configuration.py
+++ app/configuration.py
@@ -6,8 +6,8 @@ class Configuration:
         self.config = {
             "main": "C:\\Users\\Drone\\Desktop\\Panagiotis\\My-Digital-Drone-Twin\\",
             "realsense_viewer": "C:\\Users\\Public\\Desktop\\Intel RealSense Viewer.lnk",
-            "skeletal_tracking": "C:\\Users\\Drone\\Desktop\\Panagiotis\\Cycling data\\20210218_113139.bag",
-            "offline_analysis": "C:\\Users\\Drone\\Desktop\\Panagiotis\\Cycling data\\20210218_113139both.avi"
+            "skeletal_tracking": "C:\\Users\\Drone\\Desktop\\Panagiotis\\Field\\20210416_135052.bag",
+            "offline_analysis": "C:\\Users\\Drone\\Desktop\\Panagiotis\\Field\\20210416_133148.avi"
         }
     
     # This function should replace self.config in the future.
